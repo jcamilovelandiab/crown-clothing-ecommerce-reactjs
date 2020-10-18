e81d0660504fb8abaa3d71cd19f5f7dd8b5f826c
Juan Camilo <jcamilovelandiab@gmail.com>
2020-10-18 16:39:46 -0500

    updating shop data with new data and car utils to group items in cart reducer

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -2,7 +2,14 @@ import React from 'react';
 import './custom-button.styles.scss';
 
 const CustomButton = ({children, isGoogleSignIn,  inverted, ...otherProps}) =>(
-    <button className={`${isGoogleSignIn ? 'google-sign-in':''} ${inverted ? 'inverted':''} custom-button`} {...otherProps}>
+    <button
+        className={`
+            ${isGoogleSignIn ? 'google-sign-in':''}
+            ${inverted ? 'inverted':''}
+            custom-button
+        `}
+        {...otherProps}
+    >
         {children}
     </button>
 );
