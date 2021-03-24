d41255d7f0657ccb7ac507252028c3ae1ed85c4f
juan camilo velandia botello <juan.velandia@equilibrio.agency>
2021-03-23 20:03:22 -0500

    moved our directory and shop data into its own reducer, created respective selectors and updated corresponding components with new redux flow for directory/shop

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { connect } from 'react-redux';
+
 import { clearItemFromCart, addItemToCart, removeItemFromCart } from '../../redux/cart/cart.actions';
 
 import './checkout-item.styles.scss';
